c00c321d847afb4817e84336aa7db3a127a5165f
JesusQuinones17 <jesus_quinones@epam.com>
2023-10-12 00:44:57 -0600

    Conditional rendering ready for Courses component

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -3,19 +3,39 @@ import Button from "../../common/Button/Button";
 import { mockedCoursesList } from "../../constants";
 
 import './Courses.css';
+import { useState, useEffect } from "react";
+import CreateCourse from "../CreateCourse/CreateCourse";
 
 function Courses() {
+  const [showCourses, setShowCourses] = useState(true);
+  const text = showCourses ? 'Add new course' : 'Create course';
+
+  const changeComponent = () => {
+    setShowCourses(!showCourses);
+  }
+
+  useEffect(() => {
+    console.log(showCourses);
+  }, [showCourses])
+
   return (
-    <div className="courses">
+    <div className="courses-container">
       <div className="courses-right">
-        <Button buttonText="Add new course"/>
+        <Button buttonText={text} onClick={changeComponent}/>
       </div>
-      {mockedCoursesList.map((course) => {
-        console.log(course);
-        return <CourseCard key={course.id} course={course} />;
-      })}
+      {showCourses ? 
+        (
+          mockedCoursesList.map((course) => {
+          // console.log(course);
+          return <CourseCard key={course.id} course={course} />;
+          })
+        ) : (
+          <CreateCourse />
+        ) 
+      }
+       
     </div>
-  );
+  )
 }
 
 export default Courses;
---
+++ src/components/CreateCourse/CreateCourse.jsx
@@ -0,0 +1,9 @@
+function CreateCourse() {
+    return (
+        <div>
+            <h3>Hello from CreateCourse</h3>
+        </div>
+    );
+}
+
+export default CreateCourse;
