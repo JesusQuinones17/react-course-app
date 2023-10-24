acd3ad9d3224724303c8dc0c3c4665e5c1712e4e
JesusQuinones17 <jesus_quinones@epam.com>
2023-10-24 17:24:06 -0600

    Fetch courses from backend get it

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Button from "../../common/Button/Button";
 import CourseCard from "./components/CourseCard/CourseCard";
 import CreateCourse from "../CreateCourse/CreateCourse";
@@ -7,9 +8,37 @@ import './Courses.css';
 import { useState } from "react";
 
 function Courses() {
-  const [updatedCoursesList, setUpdatedCoursesList] = useState(mockedCoursesList);
+  const [updatedCoursesList, setUpdatedCoursesList] = useState([]);
   const [showCourses, setShowCourses] = useState(true);
   const text = showCourses ? 'Add new course' : 'Create course';
+
+  const apiCoursesURL = 'http://localhost:4000/courses/all';
+
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  useEffect(() => {
+    const getCourses = async () => {
+      try {
+        const response = await fetch(apiCoursesURL, requestOptions);
+        if (!response.ok) {
+          throw new Error('Get courses failed');
+        }
+        const courses = await response.json();
+        setUpdatedCoursesList(courses);
+        console.log(updatedCoursesList);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+    fetch(apiCoursesURL, requestOptions).then(resp => console.log(resp.body));
+    getCourses();
+  }, []);
+
 
   const changeComponent = () => {
     setShowCourses(!showCourses);
@@ -20,24 +49,25 @@ function Courses() {
     // console.log(typeof mockedCoursesList);
   }
 
+
   return (
     <div className="courses-container">
       <div className="courses-right">
         <Button buttonText={text} onClick={changeComponent}/>
       </div>
-      {showCourses ? 
-        (
-          updatedCoursesList.map((course) => {
-          console.log(course);
-          return <CourseCard key={course.id} course={course} />;
-          }, )
-        ) : (
-          <CreateCourse onClick={changeComponent}/>
-        ) 
-      }
-       
+      {showCourses ? (
+        <>
+          <pre>{JSON.stringify(updatedCoursesList.result[0], null, 2)}</pre>
+          
+        </>
+      ) : (
+        <CreateCourse onClick={changeComponent}/>
+      )}
+
     </div>
   )
 }
 
 export default Courses;
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import App from './App.jsx'
 import Registration from './components/Registration/Registration.jsx'
 import Login from './components/Login/Login.jsx'
+
+import { store } from './store/index.js'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='registration' element={<Registration />} />
-        <Route path='login' element={<Login />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='registration' element={<Registration />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>,
 )
